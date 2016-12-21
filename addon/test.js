02901d415d9c7e764c6cd0290a883c44a2de5c19
Darien Pardinas <dpar39@gmail.com>
2016-12-21 11:21:48 +1100

    Refactored test.js to tidy up the configuration section

--- addon/test.js
+++ addon/test.js
@@ -1,10 +1,44 @@
-
+/////////////////////////////////////////////////
+//               Configuration                 //
+/////////////////////////////////////////////////
 var engineConfigFile = '../share/config.json';
 var inImgName = '../research/sample_test_images/000.jpg';
 var outImgName = 'output.png';
 
-var fs = require("fs");
+var printDef = {
+    canvas: {
+        height: 4.0,
+        width: 6.0,
+        resolution: 300,
+        units: "inch"
+    },
+    standard: {
+        pictureWidth: 2.0,
+        pictureHeight: 2.0,
+        faceHeight: 1.1875,
+        eyesHeight: 1.25,
+        units: "inch"
+    }
+};
+/////////////////////////////////////////////////
 
+// var printDef = {
+//     canvas: {
+//         height: 5.0,
+//         width: 7.0,
+//         resolution: 300,
+//         units: "inch"
+//     },
+//     standard: {
+//         pictureWidth: 35,
+//         pictureHeight: 45,
+//         faceHeight: 34,
+//         units: "mm"
+//     }
+// };
+
+
+var fs = require("fs");
 var addon = require("./addon");
 // Create object under test
 var pppEngine = new addon.PppWrapper();
@@ -14,37 +48,6 @@ pppEngine.configure(jsonConfig);
 
 // Try to set an Image
 var imageData = fs.readFileSync(inImgName);
-
-var printdef = {
-    canvas: {
-        height: 5.0,
-        width: 7.0,
-        resolution: 300,
-        units: "inch"
-    },
-    standard: {
-        pictureWidth: 35,
-        pictureHeight: 45,
-        faceHeight: 34,
-        units: "mm"
-    }
-};
-
-printdef = {
-    canvas: {
-        height: 4.0,
-        width: 6.0,
-        resolution: 300,
-        units: "inch"
-    },
-    standard: {
-        pictureWidth: 2,
-        pictureHeight: 2,
-        faceHeight: (1 + 1+3.0/8.0)/2,
-        eyesHeight: (1+1.0/8.0 + 1+3.0/8.0)/2,
-        units: "inch"
-    }
-};
 
 pppEngine.setImage(imageData, function(err, imgKey) {
     if (err) {
@@ -57,17 +60,18 @@ pppEngine.setImage(imageData, function(err, imgKey) {
         }
         console.log(landmarks);
 
-        printdef.crownPoint = landmarks.crownPoint;
-        printdef.chinPoint = landmarks.chinPoint;
+        // Copy the estimated crown and chin points 
+        printDef.crownPoint = landmarks.crownPoint;
+        printDef.chinPoint = landmarks.chinPoint;
 
-        pppEngine.createTilePrint(imgKey, printdef, function(err, bufferData) {
+        pppEngine.createTilePrint(imgKey, printDef, function(err, bufferData) {
             if (err) {
                 throw err;
             }
-
+            console.log('Print photo created!');
             fs.writeFile(outImgName, bufferData, function(err) {
                 if (err) {
-                    return console.error(err);
+                    throw err;
                 }
             });
         });
