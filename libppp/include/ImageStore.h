d6db83754c9d214615bda9b0d8bc41cef35f914b
Darien Pardinas <dpar39@gmail.com>
2017-02-03 21:20:45 -0500

    Upgrading to C++14

--- libppp/include/ImageStore.h
+++ libppp/include/ImageStore.h
@@ -9,11 +9,11 @@ class ImageStore : public IImageStore
 {
 public:
 
-    virtual std::string setImage(const cv::Mat &image) override;
+    std::string setImage(const cv::Mat &image) override;
 
-    virtual bool containsImage(const std::string &imageKey) override;
+    bool containsImage(const std::string &imageKey) override;
 
-    virtual const cv::Mat &getImage(const std::string &imageKey) override;
+    const cv::Mat &getImage(const std::string &imageKey) override;
 private:
     std::unordered_map<std::string, cv::Mat> m_imageCollection;
    
