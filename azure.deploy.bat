3363f0e91ba953e1176ed3016f298fb7173e8dca
Darien Pardinas <dpar39@gmail.com>
2017-03-05 15:58:59 -0500

    Removing custom version of node from iisnode.yml

--- azure.deploy.bat
+++ azure.deploy.bat
@@ -1,17 +1,12 @@
-
-
-cd %~dp0\..
-
-git clone https://dpardinas@passport-photo.scm.azurewebsites.net:443/passport-photo.git azure
-
-xcopy %~dp0\webapp azure /y /d /i
-
+:: ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Commits new changes on the website and deploys to azure
+:: ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+pushd %~dp0\..
+if not exist azure (
+    git clone https://dpardinas@passport-photo.scm.azurewebsites.net:443/passport-photo.git azure    
+)
+xcopy %~dp0\webapp azure /y /e /exclude:node_modues+uploads
 cd azure
-
-git add .
-
-git commit -m "Deploy"
-
+git commit -a -m "Deploy"
 git push origin master
-
-cd %~dp0
+popd
