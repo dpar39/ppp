241eb0a87106151968e658b045784f585b6e102c
Darien Pardinas <dpar39@gmail.com>
2017-03-05 16:43:32 -0500

    Ignoring files when deploying

--- azure.deploy.bat
+++ azure.deploy.bat
@@ -1,11 +1,12 @@
 :: ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Commits new changes on the website and deploys to azure
 :: ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@echo off
 pushd %~dp0\..
 if not exist azure (
     git clone https://dpardinas@passport-photo.scm.azurewebsites.net:443/passport-photo.git azure    
 )
-xcopy %~dp0\webapp azure /y /e /exclude:node_modues+uploads
+xcopy %~dp0\webapp azure /y /s /exclude:%~dp0\webapp\.gitignore
 cd azure
 git commit -a -m "Deploy"
 git push origin master
