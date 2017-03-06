799cbec63291d1a7f7a154eaa6995753a2d1fe34
Darien Pardinas <dpar39@gmail.com>
2017-03-06 16:26:18 -0500

    Removed unused files

--- build.win32.bat
+++ build.win32.bat
@@ -1,1 +1,2 @@
 python build.py --build_config=release --arch_name=x86
+pause
--- webapp/deploy.azure.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "Deploy From git"
-git remote add azure https://dpardina@passport-photo.scm.azurewebsites.net:443/passport-photo.git
-git pull --rebase azure master
-git push azure master
