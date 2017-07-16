9f2388a2a7e6fe8d8fa4a968c00b4c88093cd21c
Darien Pardinas <dpar39@gmail.com>
2017-07-16 18:11:20 -0400

    Added cordova-ionic template

---
+++ cordova/cpp/NativeLog.h
@@ -0,0 +1,16 @@
+#ifndef NATIVELOG_H
+#define NATIVELOG_H
+
+#include <string>
+
+void _log(const char *format, ...) {
+    std::string formatStr = format;
+    formatStr += "\n";
+    
+    va_list args;
+    va_start (args, format);
+    vprintf(formatStr.c_str(), args);
+    va_end(args);
+}
+
+#endif //NATIVELOG_H
