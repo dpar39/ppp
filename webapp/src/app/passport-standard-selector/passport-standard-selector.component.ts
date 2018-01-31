96572b7466da44cbe68355967f4999c4da2371a5
Darien Pardinas <dpar39@gmail.com>
2018-01-31 17:52:35 -0500

    Work on passport-standard-selector.component.ts

--- webapp/src/app/passport-standard-selector/passport-standard-selector.component.ts
+++ webapp/src/app/passport-standard-selector/passport-standard-selector.component.ts
@@ -1,38 +1,45 @@
-import { Component, OnInit, Output } from '@angular/core';
-
+import { Component, OnInit, Output, Input } from '@angular/core';
 import { PassportStandard, UnitType } from '../model/datatypes';
 import { EventEmitter } from '@angular/core';
 
-
 @Component({
-  selector: 'app-passport-standard-selector',
-  templateUrl: './passport-standard-selector.component.html',
-  styleUrls: ['./passport-standard-selector.component.css']
+    selector: 'app-passport-standard-selector',
+    templateUrl: './passport-standard-selector.component.html',
+    styleUrls: ['./passport-standard-selector.component.css']
 })
 export class PassportStandardSelectorComponent implements OnInit {
 
-  constructor() {
-    this._standard = {
-      pictureHeight: 2.0,
-      pictureWidth: 2.0,
-      faceHeight: 1.1875,
-      units: UnitType.inch
-    };
-  }
-  photoIdType: any;
+    constructor() {
+        this._standard = {
+            pictureHeight: 2.0,
+            pictureWidth: 2.0,
+            faceHeight: 1.1875,
+            units: UnitType.inch
+        };
+    }
+    photoIdType: any;
 
-  private _standard: PassportStandard;
+    private _standard: PassportStandard;
 
-  @Output()
-  selectedStandard: EventEmitter<PassportStandard> = new EventEmitter();
+    @Output()
+    selectedStandard: EventEmitter<PassportStandard> = new EventEmitter();
 
-  ngOnInit() {
+    @Input()
+    get counter() {
+      return this._standard;
+    }
+    set photoStandard(ps: PassportStandard) {
+        this._standard = ps;
+        this.selectedStandard.emit(this._standard);
+    }
 
-    this.photoIdType = {
-      name: 'US Passport',
-      dimensions: '2\" x 2"'
-    };
-  }
+    ngOnInit() {
+
+        this.photoIdType = {
+            name: 'US Passport',
+            dimensions: '2\" x 2"'
+        };
+    }
 
 
 }
