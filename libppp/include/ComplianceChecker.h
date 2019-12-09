b3388982fe88262867d0a455db296d558307682c
Darien Pardinas Diaz <darien.pardinasdiaz@sas.com>
2019-12-08 20:00:41 -0500

    [skip ci] added some comments

--- libppp/include/ComplianceChecker.h
+++ libppp/include/ComplianceChecker.h
@@ -27,5 +27,34 @@ private:
                                               const PhotoStandardSPtr & photoStandard,
                                               const cv::Point & crownPoint,
                                               const cv::Point & chinPoint) const;
+
+    ComplianceResultSPtr checkBlurriness(const std::string & imgKey,
+                                         const PhotoStandardSPtr & photoStandard,
+                                         const cv::Point & crownPoint,
+                                         const cv::Point & chinPoint) const;
+
+    // Blurred
+    // Looking Away
+    // Ink marks/creased
+    // Unnatural skin tones
+    // Too dark/light
+    // Washed out
+    // Pixelation
+    // Hair accross eyes
+    // Eyes closed
+    // Varied background
+    // Yaw/pitch/roll greather than threshold
+    // Flash reflection on skin
+    // Red eyes
+    // Shadows behind head
+    // Shadows accross face
+    // Dark tinted lenses
+    // Flash reflection on lenses
+    // Glass frames too heavy
+    // Glass frames covering eyes
+    // Hat/Cap
+    // Veil over face
+    // Mouth open
+    // Presence of other faces or toys too close to face
 };
 } // namespace ppp
